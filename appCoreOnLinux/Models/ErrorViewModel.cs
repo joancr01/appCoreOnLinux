6f18816bfe4c58cef9532c9b47bbd93bf279b11e
joancr01 <43827381+joancr01@users.noreply.github.com>
2021-02-21 08:32:51 -0400

    Add project files.

---
+++ appCoreOnLinux/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace appCoreOnLinux.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
